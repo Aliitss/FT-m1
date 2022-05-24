0d059f94719d3349816961f5ee9fc5ddc28d7957
Aliitss <alebolivarparq@gmail.com>
2022-05-24 10:16:24 -0300

    homework 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,23 @@
 'use strict'
 
-function BinarioADecimal(num) {
+function BinarioADecimal(num) { // D * 2 ** Index (formula binario a decimal)
   // tu codigo aca
-
+let sum = 0;
+for (let i = 0; i < num.length; i++){
+  sum = sum + num[i] * 2 ** (num.length - 1 -i);
+}
+return sum;
 }
 
-function DecimalABinario(num) {
+function DecimalABinario(num) { // D / 2 %
   // tu codigo aca
-
+let str = "";
+while (num > 0){
+  let resto = num % 2;
+  str = resto + str;
+  num = Math.floor(num / 2);
+}
+return str;
 }
 
 
